c2aa57d5a71c0df03b3b44e04f66ff9e5330bc1f
jairpugliese <112570064+jairpugliese@users.noreply.github.com>
2024-11-09 16:37:35 -0500

    basededatos

--- assets/js/carrito.js
+++ assets/js/carrito.js
@@ -5,14 +5,14 @@ document.addEventListener('DOMContentLoaded', () => {
             nombre: 'iPhone 12, descripción',
             precio: 1200000,
             imagen: 'assets/img/iPhone1.png',
-            categoria: 'iPhone',
+            categoria: 'iPhone'
         },
         {
             id: 2,
             nombre: 'iPhone 13, descripción',
             precio: 12500000,
             imagen: 'assets/img/iPhone2.png'
-            categoria: 'iPhone',
+            categoria: 'iPhone'
         }
     ];
 
